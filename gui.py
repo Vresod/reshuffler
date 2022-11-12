d6adce6dfe7b270d74e247c55c290c84ec479240
Vresod <lukebinkofsky@gmail.com>
2022-11-12 01:41:34 -0500

    add gui todo list

--- gui.py
+++ gui.py
@@ -1,6 +1,13 @@
 import PySimpleGUI as sg
 import settings
 
+# GUI TODO:
+# make backend.py more modular
+# add backend.py functionality
+# make settings tab let you modify the settings
+# make colors look nicer
+# add icon, separate from python.exe, etc, basically just steal boilerplate from AUE-PY
+
 settings_layout = sg.Tab(
 	"Settings",
 	[[sg.Text(i),sg.Push(),sg.Text(settings.Settings[i] if i != "client_secret" else "*" * len(settings.Settings[i]))] for i in settings.Settings]
