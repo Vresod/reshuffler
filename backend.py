563754bd13b9c6f630e685d954342eb36ac91986
Vresod <lukebinkofsky@gmail.com>
2022-11-13 21:51:23 -0500

    make backend.py more function-based it looks ugly now but I promise this will make the GUI 1000x easier

--- backend.py
+++ backend.py
@@ -17,23 +17,38 @@ import extra
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=extra.SCOPES,client_id=Settings.client_id,client_secret=Settings.client_id,redirect_uri=Settings.client_id))
 
-playlists = sp.current_user_playlists()['items']
+def list_playlists():
+	return sp.current_user_playlists()['items']
 
-picked = extra.pick_playlist(sp,playlists)
-# picked = playlists[3]
+def get_track_list(playlist:dict) -> list:
+	results = sp.playlist_tracks(playlist['id'])
+	tracks:list = results['items']
+	while results['limit'] + results['offset'] < results['total']:
+		results = sp.playlist_tracks(playlist['id'],offset=len(tracks))
+		tracks.extend(results['items'])
+	return tracks
 
-results = sp.playlist_tracks(picked['id'])
-tracks:list = results['items']
-while results['limit'] + results['offset'] < results['total']:
-	results = sp.playlist_tracks(picked['id'],offset=len(tracks))
-	tracks.extend(results['items'])
+def create_queue(tracks:list):
+	new_queue = list[str]()
+	for i in range(Settings.added_songs):
+		track = random.choice(tracks)['track']
+		# print(i,track['artists'][0]['name']," - ",track['name'])
+		new_queue.append(track['id'])
+	return new_queue
 
-new_queue = list[str]()
-for i in range(Settings.added_songs):
-	track = random.choice(tracks)['track']
-	print(i,track['artists'][0]['name']," - ",track['name'])
-	new_queue.append(track['id'])
-print(new_queue)
+def add_to_queue(queue:list[str]):
+	for track in queue:
+		sp.add_to_queue(track)
 
-for track in new_queue:
-	sp.add_to_queue(track)
+def main():
+	playlists = list_playlists()
+
+	picked = extra.pick_playlist(sp,playlists)
+	# picked = playlists[3]
+
+	tracks = get_track_list(picked)
+	new_queue = create_queue(tracks)
+	add_to_queue(new_queue)
+
+if __name__ == "__main__":
+	pass
